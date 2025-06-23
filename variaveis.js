3ce4a04d1a1254938519d6039ad48c2ebefe7cc1
Lucas Simidu <lucas.simidu@outlook.com>
2025-06-22 21:56:12 -0300

    feat: If & function examples

--- variaveis.js
+++ variaveis.js
@@ -4,3 +4,17 @@ let aluno; //mutável
 aluno = "Caio";
 
 console.log(professor, aluno);
+
+// Exemplo de If
+if (1 > 0) {
+    console.log(`Olá, meu nome é ${professor} e meu aluno se chama ${aluno}`)
+    //variáveis 'var' possuem escopo global
+    //variáveis 'let' possuem escopo por bloco
+}
+
+// Exemplo de função
+function cumprimentar(nome) {
+    console.log(`Olá ${nome}`)
+}
+
+cumprimentar("Helena")
