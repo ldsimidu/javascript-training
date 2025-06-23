ee0d84753c5cb5b7f72d3bf6be46bae7f94c608b
Lucas Simidu <lucas.simidu@outlook.com>
2025-06-22 22:59:44 -0300

    feat: Media function

--- variaveis.js
+++ variaveis.js
@@ -14,7 +14,25 @@ if (1 > 0) {
 
 // Exemplo de função
 function cumprimentar(nome) {
-    console.log(`Olá ${nome}`)
+    console.log(`Olá ${nome.toUpperCase()}`)
 }
 
 cumprimentar("Helena")
+
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5'); //parseInt = transforma para inteiro
+
+function media(nota1, nota2, nota3, nota4) {
+    const total = (nota1 + nota2 + nota3 + nota4)/4
+    console.log(`A média total é de ${total.toFixed(2)}`)
+
+    if (total < 6) {
+        console.log("Não passou de ano")
+    } else {
+        console.log("Passou de ano")
+    }
+}
+
+media(notaPrimeiroBi, notaSegundoBi, notaTerceiroBi, notaQuartoBi)
