8b60b8b29b4472f5ddd76ec23cbde6d781dcd28c
Lucas Simidu <lucas.simidu@outlook.com>
2025-06-26 01:15:21 -0300

    feat: More boolean examples

--- boolean.js
+++ boolean.js
@@ -20,3 +20,32 @@ if ('0' == 0) {
 } else {
     console.log("Não passou na comparação")
 }
+
+let matriculaAtiva = true;
+
+// condição ? valorSeVerdadeiro : valorSeFalso
+function verificaMatriculaAtiva() {
+    return matriculaAtiva ? 'matrícula ativa no sistema' : 'matrícula não está ativa';
+}
+
+console.log(verificaMatriculaAtiva());
+
+//
+// operador ternário (? :)
+//
+
+// completo
+let idadeEstudante = 16;
+let precisaDeAutorizacao;
+
+if (idadeEstudante < 18) {
+ precisaDeAutorizacao = true;
+} else {
+ precisaDeAutorizacao = false;
+}
+
+console.log(precisaDeAutorizacao);
+
+// simples
+const autorizacao = idadeEstudante < 18 ? true : false;
+console.log(autorizacao)
