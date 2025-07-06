3a2aceecae016e61a70d196f9874cd386ce2488c
Lucas Simidu <lucas.simidu@outlook.com>
2025-07-06 02:19:49 -0300

    feat: Copy & Set examples (React w/useState)

---
+++ aula 03 - advanced array/copy (RC)/copy.js
@@ -0,0 +1,7 @@
+const pilotosF1 = ["Charles Leclerc", "Lian Lawson", "Lando Norris", "Lewis Hamilton", "Max Verstappen"] 
+const novosPilotosF1 = [...pilotosF1, "Gabriel Bortoleto"] //ABRE O ARRAY E ESPALHA O CONTEUDO
+
+//novosPilotosF1.push("Gabriel Bortoleto")
+
+console.log(pilotosF1);
+console.log(novosPilotosF1);
---
+++ aula 03 - advanced array/set (RC)/set1.js
@@ -0,0 +1,4 @@
+const pilotosF1 = ["Charles Leclerc", "Lian Lawson", "Lando Norris", "Lewis Hamilton", "Lando Norris", "Max Verstappen", "Lian Lawson"] 
+
+const pilotosAtualizados = new Set(pilotosF1)
+console.log(pilotosAtualizados);
