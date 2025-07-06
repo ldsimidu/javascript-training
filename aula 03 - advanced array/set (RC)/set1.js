c23a6587952d3f223080da4a577032c0e1339418
Lucas Simidu <lucas.simidu@outlook.com>
2025-07-06 02:26:34 -0300

    fix: Simplifying new set (RC)

--- aula 03 - advanced array/set (RC)/set1.js
+++ aula 03 - advanced array/set (RC)/set1.js
@@ -1,4 +1,7 @@
 const pilotosF1 = ["Charles Leclerc", "Lian Lawson", "Lando Norris", "Lewis Hamilton", "Lando Norris", "Max Verstappen", "Lian Lawson"] 
 
-const pilotosAtualizados = new Set(pilotosF1)
-console.log(pilotosAtualizados);
+//const pilotosAtualizados = new Set(pilotosF1)
+//const listaNomesAutalizados = [...pilotosAtualizados]
+const listaPilotosAutalizados = [...new Set(pilotosF1)]
+
+console.log(listaPilotosAutalizados);
